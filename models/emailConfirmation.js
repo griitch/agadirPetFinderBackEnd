34a8cd816dcbdaa8c9deb2a36bd46798b6d65bfb
omar <omargrinat9@gmail.com>
2022-02-08 20:32:24 +0100

    add mongoose models

---
+++ models/emailConfirmation.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const emailConfirmation = mongoose.Schema({
+  confirmationToken: {
+    type: Number,
+    required: true,
+  },
+
+  post: {
+      type : mongoose.Schema.Types.ObjectId, ref = "Post"
+  },
+});
+
+module.exports = mongoose.model("EmailConfirmation", emailConfirmation);
