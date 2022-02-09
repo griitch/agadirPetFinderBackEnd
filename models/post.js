891c4e24e6ea3a976b4ac7cb10f8eaf69f786d4b
omar <omargrinat9@gmail.com>
2022-02-09 20:11:52 +0100

    add timestamp option in model

--- models/post.js
+++ models/post.js
@@ -1,63 +1,69 @@
 const mongoose = require("mongoose");
 const neighborhoods = require("../lib/neighborhoods");
 
-const postSchema = new mongoose.Schema({
-  animal: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["chat", "chien"],
-      message: "animal {VALUE} is not supported",
+const postSchema = new mongoose.Schema(
+  {
+    animal: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["chat", "chien"],
+        message: "animal {VALUE} is not supported",
+      },
+    },
+
+    breed: { type: String, default: "non reconnu" },
+
+    gender: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["femelle", "male"],
+        message: "gendre {VALUE} is not supported",
+      },
+    },
+
+    neighborhood: {
+      type: String,
+      enum: {
+        values: neighborhoods,
+        message: "neighborhood {VALUE} is not supported",
+      },
+      required: true,
+    },
+
+    confirmed: { type: Boolean, default: false },
+
+    email: {
+      type: String,
+      required: true,
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    },
+
+    phoneNumber: {
+      type: String,
+      required: true,
+      match: [/^0[6,7][0-9]{8}/, "Numéro de téléphone invalide"],
+    },
+
+    picUrl: {
+      type: String,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png",
+    },
+
+    picFileId: String,
+
+    description: {
+      type: String,
+      maxlength: 200,
     },
   },
-
-  breed: { type: String, default: "non reconnu" },
-
-  gender: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["femelle", "male"],
-      message: "gendre {VALUE} is not supported",
-    },
-  },
-
-  neighborhood: {
-    type: String,
-    enum: {
-      values: neighborhoods,
-      message: "neighborhood {VALUE} is not supported",
-    },
-    required: true,
-  },
-
-  confirmed: { type: Boolean, default: false },
-
-  email: {
-    type: String,
-    required: true,
-    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-  },
-
-  phoneNumber: {
-    type: String,
-    required: true,
-    match: [/^0[6,7][0-9]{8}/, "Numéro de téléphone invalide"],
-  },
-
-  picUrl: {
-    type: String,
-    default:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png",
-  },
-
-  picFileId: String,
-
-  description: {
-    type: String,
-    maxlength: 200,
-  },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 postSchema.methods.markAsConfirmed = function () {
   return mongoose
